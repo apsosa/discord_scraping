4808f455f5fbc5c81aa121a3b6c5b8a6ea5e3f39
apsosa <patdeveloper.data@gmail.com>
2021-11-11 20:48:50 -0300

    scrapping algo31

---
+++ boot.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def retrieve_messages(channel_id,file_name):
+  headers = {
+    'authorization': 'NTM1MjAzNjE2NzQzNjIwNjEx.YY2eJA.a5jJN6LAtEjPbd7_2oGntri9Qqg'
+  }
+  r = requests.get(f'https://discord.com/api/v9/channels/{channel_id}/messages',headers=headers)
+  jsonn = json.loads(r.text)
+  text_file = open(file_name, "w")
+  for value in jsonn:
+    text_file.write(value['author']['username']+':'+value['content']+'\n')
+    print(value, '\n')
+  text_file.close()
+
+
+file_name1 = "practica1.txt" 
+file_name2 = "practica2.txt" 
+file_name3 = "practica3.txt" 
+file_name4 = "practica4.txt" 
+file_name5 = "practica5.txt" 
+file_name6 = "practica6.txt"  
+retrieve_messages(id_practica1,file_name1)
+retrieve_messages(id_practica2,file_name2)
+retrieve_messages(id_practica3,file_name3)
+retrieve_messages(id_practica4,file_name4)
+retrieve_messages(id_practica5,file_name5)
+retrieve_messages(id_practica6,file_name6)
